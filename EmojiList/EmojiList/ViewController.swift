fb14a99afda2e0ae89b09a7a9c07627a6af72862
AlexZhadaev <pvt.vatnik@gmail.com>
2021-05-03 20:28:34 +0300

    Project created

---
+++ EmojiList/EmojiList/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  EmojiList
+//
+//  Created by Жадаев Алексей on 03.05.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
