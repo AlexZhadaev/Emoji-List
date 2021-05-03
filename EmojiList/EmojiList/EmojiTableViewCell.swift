29769ad4930015ff24858b831e4799c8ec5aa4fe
AlexZhadaev <pvt.vatnik@gmail.com>
2021-05-03 21:54:15 +0300

    upd #1

---
+++ EmojiList/EmojiList/EmojiTableViewCell.swift
@@ -0,0 +1,22 @@
+//
+//  EmojiTableViewCell.swift
+//  EmojiList
+//
+//  Created by Жадаев Алексей on 03.05.2021.
+//
+
+import UIKit
+
+class EmojiTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var emojiLabel: UILabel!
+    @IBOutlet weak var nameEmojiLabel: UILabel!
+    @IBOutlet weak var descriptionEmojiLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        
+    }
+
+
+}
